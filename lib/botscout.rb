84b402c864dd017185a2aa3c48c3b5ff50660214
Pechnikov Maxim <parallel588@gmail.com>
2013-06-06 21:26:27 +0400

    init gem

---
+++ lib/botscout.rb
@@ -0,0 +1,91 @@
+require "botscout/version"
+
+module Botscout
+  class Client
+    include HTTParty
+    format :xml
+    base_uri 'http://botscout.com'
+
+    attr_accessor :key
+    def initialize(key = "")
+      @key = key
+    end
+
+    def test(options ={})
+      _options = (@key.present? ? { :key => @key } : {}).merge(options)
+      @response = self.class.get('/test', :query => _options)
+
+      Result.new(@response.body)
+    end
+
+    class Result
+      attr_accessor :response, :result, :matched
+
+      def initialize(xml_response = "")
+        @response = xml_response
+        parse!
+      end
+
+      def parse!
+        if has_error?
+          @result = nil
+        else
+          results  = @response.split "|"
+          @data =
+            case results[1].to_s.downcase
+            when "all" then parse_all(results)
+            when "multi" then parse_multi(results)
+            else
+              parse_standart(results)
+            end
+
+          @result = @data[:matched]
+        end
+
+        self
+      end
+
+      def bot?
+        @result.to_s == "Y"
+      end
+
+      def success?
+        !has_error?
+      end
+
+      def error
+        @response.match(/\A!(.*)/) && $1
+      end
+
+      def has_error?
+        !!@response.match(/\A!/)
+      end
+
+      private
+      def parse_standart(response_array)
+        {
+          :count => response_array[2].to_i,
+          :field => response_array[1],
+          :matched => response_array[0]
+        }
+      end
+
+      def parse_all(response_array)
+        {
+          :count => response_array[2].to_i,
+          :field => response_array[3],
+          :matched => response_array[0]
+        }
+      end
+
+      def parse_multi(response_array)
+        {
+          :count => response_array[2..-1].map(&:to_i).sum,
+          :fields => Hash[*response_array[2..-1]],
+          :matched => response_array[0]
+        }
+      end
+
+    end
+  end
+end
---
+++ lib/botscout/version.rb
@@ -0,0 +1,3 @@
+module Botscout
+  VERSION = "0.0.1"
+end
